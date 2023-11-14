d517dad22b0e058a988614e4b5e2cdbd10fb715a
Noel Lazaro <44674847+noelj@users.noreply.github.com>
2023-11-14 15:43:59 -0500

    .

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+
+
+const port= process.env.PORT || 5000;
+
+
+
+const app=express();
+
+
+
+app.listen(port,async()=>{
+    console.log('Server Started Now!!')
+})
